6f3e8523ddca4cea0683fabe6278a186279d8b21
marllomartin <marllon3636@gmail.com>
2022-07-02 21:11:57 -0300

    feat(src/server.js): Implement create route

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const app = require('./app');
 
-const { getAll, getById } = require('./controllers/task.controller');
+const { getAll, getById, create } = require('./controllers/task.controller');
 
 const port = process.env.PORT
 
 app.get('/task', getAll);
 app.get('/task/:id', getById);
+app.post('/task', create);
 
 app.listen(port, () => console.log('Ouvindo porta', port));
