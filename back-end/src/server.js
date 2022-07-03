27d603ed1bcff1c570738547fb9857e2257b809b
marllomartin <marllon3636@gmail.com>
2022-07-02 21:08:50 -0300

    feat(src/server.js): Implement getById route

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const app = require('./app');
 
-const { getAll } = require('./controllers/task.controller');
+const { getAll, getById } = require('./controllers/task.controller');
 
 const port = process.env.PORT
 
 app.get('/task', getAll);
+app.get('/task/:id', getById);
 
 app.listen(port, () => console.log('Ouvindo porta', port));
