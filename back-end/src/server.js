40012cfb473ed4dd9e2336a59e91c4105ca496c0
marllomartin <marllon3636@gmail.com>
2022-07-02 21:18:59 -0300

    feat(src/server.js): Implement deleteById route

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const app = require('./app');
 
-const { getAll, getById, create } = require('./controllers/task.controller');
+const { getAll, getById, create, deleteById } = require('./controllers/task.controller');
 
 const port = process.env.PORT
 
 app.get('/task', getAll);
 app.get('/task/:id', getById);
 app.post('/task', create);
+app.delete('/task/:id', deleteById);
 
 app.listen(port, () => console.log('Ouvindo porta', port));
