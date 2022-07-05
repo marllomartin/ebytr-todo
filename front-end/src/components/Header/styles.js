27211ee5457efeda7a0060a90aca0e51231c9548
marllomartin <marllon3636@gmail.com>
2022-07-04 22:30:48 -0300

    feat(src/components): Create Styled Component Header

---
+++ front-end/src/components/Header/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  width: 100vw;
+  height: 50px;
+  padding: 0px 20px;
+  margin-top: 10px;
+
+  display: flex;
+  align-items: center;
+
+  & h3 {
+    font-size: 30px;
+    text-align: center;
+    color: #E2E0CA;
+  }
+`;
